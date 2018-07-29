cf03410742203bbba67a854ba77cfbbf1d935b1b
Sreenidish Chinmayanilayam <schinmayanilayam@salesforce.com>
2018-07-29 09:25:15 -0700

    scatter chart added.

--- viz.js
+++ viz.js
@@ -56,7 +56,7 @@ var formatDateIntoYear = d3.timeFormat("%Y");
 var formatDate = d3.timeFormat("%b %Y");
 
 var startDate = new Date("2010-01-01"),
-    endDate = new Date("2017-12-31");
+    endDate = new Date("2015-12-31");
 
 var width = gWidth,
     height = gHeight;
@@ -85,7 +85,17 @@ slider.append("line")
     .attr("class", "track-overlay")
     .call(d3.drag()
         .on("start.interrupt", function() { slider.interrupt(); })
-        .on("start drag", function() { hue(x.invert(d3.event.x)); }));
+        .on("start drag", function() {
+          //console.log("x.invert(d3.event.x): " + formatDateIntoYear(x.invert(d3.event.x)) );
+          //console.log("d3.event.x: " + d3.event.x );
+          h = x.invert(d3.event.x);
+          handle.attr("cx", x(h));
+          //label
+          //    .attr("x", x(h))
+          //    .text(formatDate(h));
+          updateView(formatDateIntoYear(h), triggerSelected ) }));
+
+//        .on("start drag", function() { hue(x.invert(d3.event.x));}));
 
 slider.insert("g", ".track-overlay")
     .attr("class", "ticks")
@@ -98,17 +108,19 @@ slider.insert("g", ".track-overlay")
     .attr("y", 10)
     .attr("text-anchor", "middle")
     .text(function(d) { return formatDateIntoYear(d); });
-
+/*
 var label = slider.append("text")
     .attr("class", "label")
     .attr("text-anchor", "middle")
     .text(formatDate(startDate))
     .attr("transform", "translate(0," + (-25) + ")")
+*/
 
 var handle = slider.insert("circle", ".track-overlay")
     .attr("class", "handle")
     .attr("r", 9);
 
+    /*
 function hue(h) {
   handle.attr("cx", x(h));
   label
@@ -116,6 +128,7 @@ function hue(h) {
     .text(formatDate(h));
   svg.style("background-color", d3.hsl(h/1000000000, 0.8, 0.8));
 }
+*/
 
 }
 
@@ -133,25 +146,36 @@ function isRealValue(obj) {
  return obj && obj !== 'null' && obj !== 'undefined';
 }
 
-function svgContainerInit() {
+function svgContainerInit(action) {
     //d3.select(".abtVizMsg").style("visibility", "hidden");
+
     svgContainer =  d3.select(".dispContainer").select("svg");
     svgContainer.attr("width", gWidth).attr("height", gHeight);
-    svgContainer.selectAll("*").remove();
+    console.log("In svgContainerInit(): action = " + action );
+
+      switch (action) {
+      case "clear":
+        svgContainer.selectAll("*").remove();
+        break;
+      default:
+         console.log("In svgContainerInit: Nothing to do.");
+    }
 }
 
-function updateView(featureToDisplay="", yearToDisplay=2015) {
+function updateView(yearToDisplay=2015,featureToDisplay="") {
   var travYear = yearToDisplay;
 
-    svgContainerInit();
   var sequentialButtons = d3.select(".featuresButtons")
     .selectAll("button")
     .data(analysisFeaturesMenu.features)
     .enter().append("button")
     .text(function(d) { return d.key; })
     .on("click", function(buttonValue) {
-        triggerSelected=buttonValue.value;
+        triggerSelected=buttonValue.key;
         console.log("triggerSelected: " + triggerSelected);
+        console.log("Calling displayTheScene following features button click");
+        displayTheScene(buttonValue.key);
+/*
         switch (buttonValue.key) {
         case "1":
           overview(svgContainer);
@@ -166,15 +190,38 @@ function updateView(featureToDisplay="", yearToDisplay=2015) {
           debugMsg("buttonKey is: " + buttonValue.key);
           break;
         }
+*/
       });
 
       if (!isRealValue(triggerSelected)) {
-        debugMsg("Going to call displayAboutViz()");
-        displayAboutVizBtn(svgContainer);
+        console.log("Calling displayAboutViz()");
+        displayAboutVizBtn();
+      } else {
+        displayTheScene(triggerSelected);
       }
 }
 
-function displayAboutVizBtn(svgContainer) {
+function displayTheScene(choice) {
+  console.log("Validating svgContainer: " + isRealValue(svgContainer));
+  console.log ("In displayTheScene: value of choice is: " + choice );
+  svgContainerInit("clear");
+
+  switch (choice) {
+    case "1":
+      overview();
+      break;
+    case "2":
+      incomepoverty();
+      break;
+    case "3":
+      racialdist();
+      break;
+    default:
+      console.log("displayScene could not be rendered. The choice was: " + choice);
+      break;
+    }
+}
+function displayAboutVizBtn() {
   //d3.select(".abtVizMsg").style("visibility", "visible");
 
   var displayAboutBtn = d3.select(".abtVizMsg")
@@ -188,7 +235,7 @@ function displayAboutVizBtn(svgContainer) {
 }
 
 function displayAboutVizMessage() {
-  debugMsg("Inside displayAboutVizMessage()");
+  console.log("Inside displayAboutVizMessage()");
   d3.select(".abtVizMsg").style("visibility", "visible");
 
 /*
@@ -309,11 +356,10 @@ d3.select(".abtVizMsg")
 
 
 
-function overview(svgContainer) {
+function overview() {
 var width = canvasWidth,
     height = canvasHeight;
-//debugMsg("Inside Overview");
-
+console.log("Inside Overview");
 
 var mapColor = d3.scaleLinear()
                          .range(['#ffffd4','#fee391','#fec44f','#fe9929','#d95f0e','#993404']);
@@ -369,13 +415,147 @@ slider(svgContainer);
 }
 
 
+function incomepoverty(){
 
-function incomepoverty(svgContainer) {
+  var width = canvasWidth,
+  height = canvasHeight-60;
 
-var width = gWidth,
-    height = gHeight;
+  var lmargin = {top: 20, right: 20, bottom: 100, left: 40},
+  width = canvasWidth - lmargin.left - lmargin.right,
+  height = canvasHeight - lmargin.top - lmargin.bottom;
 
-debugMsg("Inside IncomePoverty");
+var x = d3.scaleLinear()
+  .range([0, width]);
+
+var y = d3.scaleLinear()
+  .range([height, 0]);
+
+var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+/*
+var color = d3.scaleOrdinal()
+  .domain(myData)
+  .range(d3.schemePaired);
+
+*/
+
+var xAxis = d3.axisBottom(x);
+
+var yAxis = d3.axisLeft(y);
+
+/*
+var svg = d3.select("body").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+
+  */
+var svg = svgContainer
+.append("g")
+  .attr("transform", "translate(" + lmargin.left + "," + lmargin.top + ")");
+
+
+  var div = d3.select("body").append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0);
+
+  data=null;
+
+  d3.csv(fileKeyStatsByStateURL, function(error, data) {
+
+    data.forEach(function(d) {
+      d.IncomePerCap = parseInt(d.IncomePerCap);
+      d.Unemployment = parseInt(d.Unemployment);
+      d.Poverty = parseInt(d.Poverty);
+      d.ChildPoverty = parseInt(d.ChildPoverty);
+    });
+
+
+
+x.domain(d3.extent(data, function(d) { return d.IncomePerCap; })).nice();
+y.domain(d3.extent(data, function(d) { return d.Poverty; })).nice();
+color.domain(data, function (d) {return d.Unemployment;});
+
+// Circle size (Represents Child Poverty)
+var cirRadValue = function(d) { return d["ChildPoverty"];},
+cirRadScale = d3.scaleLinear().range([3.5, 3.8]),
+cirRadMap = function(d) { return cirRadScale(cirRadValue(d));};
+
+
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis)
+  .append("text")
+    .attr("class", "label")
+    .attr("x", width)
+    .attr("y", -6)
+    .style("text-anchor", "end")
+    .text("Per Capita Income");
+
+svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+  .append("text")
+    .attr("class", "label")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Poverty (%)")
+
+svg.selectAll(".dot")
+    .data(data)
+  .enter().append("circle")
+    .attr("class", "dot")
+    //.attr("r", 3.5)
+    .attr("r", cirRadMap)
+    .attr("cx", function(d) { return x(d.IncomePerCap); })
+    .attr("cy", function(d) { return y(d.Poverty); })
+    .style("fill", function(d) { return color(d.Unemployment); })
+    .on("mouseover", function(d) {
+      div.transition()
+        .duration(200)
+        .style("opacity", .9);
+      div.html(d["State"])
+        .style("left", (d3.event.pageX) + "px")
+        .style("top", (d3.event.pageY - 28) + "px");
+      })
+    .on("mouseout", function(d) {
+      div.transition()
+        .duration(500)
+        .style("opacity", 0);
+      });
+
+var legend = svg.selectAll(".legend")
+    .data(color.domain())
+  .enter().append("g")
+    .attr("class", "legend")
+    .attr("transform", function(d, i) { return "translate(0," + i * 10 + ")"; });
+
+legend.append("rect")
+    .attr("x", width - 18)
+    .attr("width", 18)
+    .attr("height", 18)
+    .style("fill", color);
+
+legend.append("text")
+    .attr("x", width - 24)
+    .attr("y", 9)
+    .attr("dy", ".35em")
+    .style("text-anchor", "end")
+    .text(function(d) { return d; });
+
+});
+slider(svgContainer);
+
+}
+
+function oldincomepoverty() {
+
+  var width = canvasWidth,
+      height = canvasHeight;
+
+console.log("Inside IncomePoverty");
 
 var xValue = function(d) { return d.IncomePerCap;},
     xScale = d3.scaleLinear().range([0, width]),
@@ -395,17 +575,18 @@ var cValue = function(d) { return d.Unemployment;},
 
 // Circle size (Represents Child Poverty)
 var cirRadValue = function(d) { return d["ChildPoverty"];},
-    cirRadScale = d3.scaleLinear().range([3.5, 3.5]),
+    cirRadScale = d3.scaleLinear().range([3.5, 3.6]),
     cirRadMap = function(d) { return cirRadScale(cirRadValue(d));};
 
 
 var svg = svgContainer
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+    //.attr("width", width + margin.left + margin.right)
+    //.attr("height", height + margin.top + margin.bottom)
   .append("g")
+    //.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var tooltip = d3.select("body").append("div")
+var tooltip = svgContainer.append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
@@ -416,7 +597,6 @@ d3.csv(fileKeyStatsByStateURL, function(error, data) {
     d.Unemployment = +d.Unemployment;
     d.Poverty = +d.Poverty;
     d.ChildPoverty = +d.ChildPoverty;
-//    console.log(d);
   });
 
   xScale.domain([d3.min(data, xValue)-1, d3.max(data, xValue)+1]);
@@ -442,7 +622,7 @@ d3.csv(fileKeyStatsByStateURL, function(error, data) {
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Poverty");
+      .text("Poverty %");
 
   // draw dots
   svgContainer.selectAll(".dot")
@@ -453,15 +633,17 @@ d3.csv(fileKeyStatsByStateURL, function(error, data) {
       //.attr("r", 3.5)
       .attr("cx", xMap)
       .attr("cy", yMap)
-      .style("fill", function(d) { return color(cValue(d));})
+      .style("fill", function(d) { return color(cValue(d.Unemployment));})
       .on("mouseover", function(d) {
           tooltip.transition()
                .duration(200)
                .style("opacity", .9);
           tooltip.html(d["State"] + "<br/> (" + xValue(d)
 	        + ", " + yValue(d) + ")")
-               .style("left", (d3.event.pageX + 5) + "px")
-               .style("top", (d3.event.pageY - 28) + "px");
+               .style("left", (d3.event.pageX) + "px")
+               .style("top", (d3.event.pageY) + "px");
+
+               //.style("top", (d3.event.pageY - 28) + "px");
       })
       .on("mouseout", function(d) {
           tooltip.transition()
@@ -495,9 +677,9 @@ slider(svgContainer);
 }
 
 
-function racialdist(svgContainer) {
-  var width = gWidth,
-    height = gHeight;
+function racialdist() {
+  var width = canvasWidth,
+      height = canvasHeight;
 
     console.log("width: " + width  + " <end>");
     console.log("height: " + height  + " <end>");
@@ -512,10 +694,6 @@ function racialdist(svgContainer) {
     //height = 100 - margin.top - margin.bottom;
 
 //document.getElementById("debug2").innerHTML = "Racial Distribution";
-
-var svgContainer = d3.select(".dispContainer").select("svg");
-svgContainer .attr("width", width).attr("height", height);
-svgContainer.selectAll("*").remove();
 
 var totalPopArr=[];
 var columns=null;
